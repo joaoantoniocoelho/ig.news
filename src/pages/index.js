ab23a004ad27f33ef9d1129dc2b27f425487fe5c
João Coelho <joaoantonioscoelho@gmail.com>
2023-01-04 22:27:51 -0300

    first commit

---
+++ src/pages/index.js
@@ -0,0 +1,10 @@
+import Head from 'next/head'
+
+export default function Home() {
+  return (
+      <>
+        <Head>Home | ig.news</Head>
+        <h1>Hello, <span>world.</span></h1>
+      </>
+  )
+}
