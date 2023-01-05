0f8097b8c4d8d702e62ccf57e70b5fc6d8283ec8
João Coelho <joaoantonioscoelho@gmail.com>
2023-01-05 02:34:45 -0300

    use ssg instead of ssr to get price

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,7 +1,7 @@
 import Head from 'next/head'
 import styles from './home.module.scss';
 import SubscribeButton from "../components/SubscribeButton";
-import {GetServerSideProps} from "next";
+import {GetStaticProps} from "next";
 import {stripe} from "../services/stripe";
 import {formatCurrency} from "../utils/formatCurrency";
 
@@ -33,7 +33,7 @@ export default function Home({product}: HomeProps) {
     )
 }
 
-export const getServerSideProps: GetServerSideProps = async () => {
+export const getStaticProps: GetStaticProps = async () => {
     const price = await stripe.prices.retrieve('price_1MMlSdCwIvAKwdKYDAaGZpLz');
 
     const product = {
@@ -44,7 +44,8 @@ export const getServerSideProps: GetServerSideProps = async () => {
     return {
         props: {
             product
-        }
+        },
+        revalidate: 60 * 60 * 24, // 24 hours
     }
 }
 
